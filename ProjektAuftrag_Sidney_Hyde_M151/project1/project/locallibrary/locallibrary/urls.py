77ea5b54eeedfee1c96003704a5d6bd11e096ed5
Sidney Hyde <shyde@online.gibz.ch>
2023-06-23 08:19:59 +0200

    Add redirect

--- ProjektAuftrag_Sidney_Hyde_M151/project1/project/locallibrary/locallibrary/urls.py
+++ ProjektAuftrag_Sidney_Hyde_M151/project1/project/locallibrary/locallibrary/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from catalog import views as catalog_views
 from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,5 +31,5 @@ urlpatterns += [
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', catalog_views.register, name='register'),
-
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
